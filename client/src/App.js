9e8c93b3ecd0722b57d85924cee3dd85ba33445e
Mozahedul <mozahed001@gmail.com>
2021-12-07 00:28:20 +0600

    Install and setup react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
 
 const App = () => {
-  return <div>React app</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
