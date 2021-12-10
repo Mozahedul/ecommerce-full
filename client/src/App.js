1c724185cb0c72d13da4dfa83b61ee360f756dff
Mozahedul <mozahed001@gmail.com>
2021-12-10 16:54:39 +0600

    Firebase authentication state

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,8 +8,33 @@ import Register from './pages/auth/Register';
 import Home from './pages/Home';
 import Header from './components/nav/Header';
 import RegisterComplete from './pages/auth/RegisterComplete';
+import { auth } from './firebase';
 
 const App = () => {
+  const dispatch = useDispatch();
+  // to check the firebase auth state
+  useEffect(() => {
+    // with onAuthStateChanged() method, we can subscribe users to
+    // current authentication state
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        // getIdTokenResult() is a method can be be used
+        // to access the protected routes from backend
+        const idTokenResult = await user.getIdTokenResult();
+        console.log('User', user);
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+
+    // cleanup
+    return () => unsubscribe();
+  }, [dispatch]);
   return (
     <>
       <Router>
