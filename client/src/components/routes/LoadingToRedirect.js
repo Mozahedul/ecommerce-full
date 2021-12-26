4579b92b735521e87f63400a6e4800a1412e815c
Mozahedul <mozahed001@gmail.com>
2021-12-26 21:26:19 +0600

    Add countdown timer and redirecting to home page

---
+++ client/src/components/routes/LoadingToRedirect.js
@@ -0,0 +1,31 @@
+import { Col, Row } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const LoadingToRedirect = () => {
+  const [count, setCount] = useState(5);
+  const history = useHistory();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((currentCount) => --currentCount);
+    }, 1000);
+    count === 0 && history.push('/');
+    return () => clearInterval(interval);
+  }, [count, history]);
+  return (
+    <p
+      className="text-info"
+      style={{
+        fontWeight: '500',
+        fontSize: '16px',
+        marginTop: '30px',
+        textAlign: 'center',
+      }}
+    >
+      Redirecting you in {count} seconds
+    </p>
+  );
+};
+
+export default LoadingToRedirect;
