4430b8e1112508ae04283edc6b95f1b13d689e7b
Mozahedul <mozahed001@gmail.com>
2021-12-29 21:15:46 +0600

    Create category route & category controller

---
+++ server/controllers/category.js
@@ -0,0 +1,21 @@
+const Category = require('../models/category');
+
+module.exports.create = (req, res) => {
+  //  
+}
+
+module.exports.list = (req, res) => {
+  //
+}
+
+module.exports.read = (req, res) => {
+  //
+}
+
+module.exports.update = (req, res) => {
+  //
+}
+
+module.exports.remove = (req, res) => {
+  //
+}
