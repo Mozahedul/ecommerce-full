2dd93e8ab929033dca8da842f07ff2dff5ec05af
Mozahedul <mozahed001@gmail.com>
2021-12-30 10:52:37 +0600

    Save a category(create category)

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,21 +1,30 @@
+const slugify = require('slugify');
+
 const Category = require('../models/category');
 
-module.exports.create = (req, res) => {
-  //  
-}
+module.exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = await new Category({ name, slug: slugify(name) }).save();
+    res.status(200).json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send('Create category failed');
+  }
+};
 
-module.exports.list = (req, res) => {
+module.exports.list = async (req, res) => {
   //
-}
+};
 
-module.exports.read = (req, res) => {
+module.exports.read = async (req, res) => {
   //
-}
+};
 
-module.exports.update = (req, res) => {
+module.exports.update = async (req, res) => {
   //
-}
+};
 
-module.exports.remove = (req, res) => {
+module.exports.remove = async (req, res) => {
   //
-}
+};
