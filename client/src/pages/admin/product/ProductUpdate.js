663d6a4de0f653a2a3d32b814b41821aa1505388
Mozahedul <mozahed001@gmail.com>
2022-01-14 22:21:31 +0600

    Get product in the frontend from backend

--- client/src/pages/admin/product/ProductUpdate.js
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import AdminNav from '../../../components/nav/AdminNav';
+import { getProduct } from '../../../functions/product';
 // import { useParams } from 'react-router-dom';
+
+const initialState = {
+  title: '',
+  description: '',
+  price: '',
+  categories: [],
+  category: '',
+  subs: [],
+  shipping: '',
+  quantity: '',
+  images: [],
+  colors: ['Black', 'Brown', 'Silver', 'White', 'Blue'],
+  brands: ['Apple', 'Samsung', 'Microsoft', 'Lenovo', 'ASUS'],
+  color: '',
+  brand: '',
+};
 
 const ProductUpdate = ({ match }) => {
   const { user } = useSelector(state => ({ ...state }));
+  const [values, setValues] = useState(initialState);
+
+  console.log(values);
 
   // const { slug } = useParams();
   // console.log(useParams());
 
   const { slug } = match.params;
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  const loadProduct = () => {
+    getProduct(slug).then(p => {
+      // console.log('single product ==> ', p);
+      setValues({ ...values, ...p.data });
+    });
+  };
 
   return (
     <div className="container-fluid">
@@ -19,7 +50,7 @@ const ProductUpdate = ({ match }) => {
         </div>
         <div className="col-md-10">
           <h4>Product Update</h4>
-          {JSON.stringify(slug)}
+          {JSON.stringify(values)}
           <hr />
         </div>
       </div>
