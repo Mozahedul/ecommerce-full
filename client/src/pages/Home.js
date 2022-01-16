8480552ed9881e6ab0e26b48c0e4acb9c36bcbf2
Mozahedul <mozahed001@gmail.com>
2022-01-16 13:33:54 +0600

    Show product in homw page in frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'antd';
+import { getProductsByCount } from '../functions/product';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  console.log(products);
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    getProductsByCount(2).then(res => {
+      setProducts(res.data);
+    });
+  };
+
   return (
     <Row>
       <Col span={22} offset={1}>
         <h3 className="m-t-2">Home Page</h3>
+        {/* {JSON.stringify(products)} */}
       </Col>
     </Row>
   );
