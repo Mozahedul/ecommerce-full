5b2d601735550b887e6ae1d553b67c2b3158189d
Mozahedul <mozahed001@gmail.com>
2022-01-16 22:53:12 +0600

    Show loading effect in home page of the frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Row } from 'antd';
 import { getProductsByCount } from '../functions/product';
 import ProductCard from '../components/cards/ProductCard';
 import Jumbotron from '../components/cards/Jumbotron';
+import LoadingCard from '../components/cards/LoadingCard';
 
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  console.log(products);
 
   useEffect(() => {
     loadAllProducts();
@@ -32,13 +30,17 @@ const Home = () => {
       </div>
 
       <div className="container">
-        <div className="row">
-          {products.map(product => (
-            <div className="col-md-4" key={product._id}>
-              <ProductCard product={product} />
-            </div>
-          ))}
-        </div>
+        {loading ? (
+          <LoadingCard count={3} />
+        ) : (
+          <div className="row">
+            {products.map(product => (
+              <div className="col-md-4" key={product._id}>
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
