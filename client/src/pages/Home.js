7ae5a66c345927aa3294fbd0fb51dfba89f164cd
Mozahedul <mozahed001@gmail.com>
2022-01-17 12:56:22 +0600

    Show Best Seller products according to sold in Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Jumbotron from '../components/cards/Jumbotron';
+import BestSellers from '../components/home/BestSellers';
 import NewArrivals from '../components/home/NewArrivals';
 
 const Home = () => {
@@ -12,11 +13,22 @@ const Home = () => {
         <Jumbotron text={['Latest Product', 'New Arrivals', 'Best Sellers']} />
       </div>
 
-      <div className="jumbotron" style={{ textAlign: 'center' }}>
+      <div
+        className="jumbotron"
+        style={{ textAlign: 'center', marginTop: '30px' }}
+      >
         <h4 className="display-4">New Arrivals</h4>
       </div>
 
       <NewArrivals />
+
+      <div
+        className="jumbotron"
+        style={{ textAlign: 'center', marginTop: '30px' }}
+      >
+        <h4 className="display-4">Best Sellers</h4>
+      </div>
+      <BestSellers />
     </>
   );
 };
