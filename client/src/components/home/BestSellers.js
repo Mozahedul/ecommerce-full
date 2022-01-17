a268aebf5169b57f715a8317bd79bef7080d7dbd
Mozahedul <mozahed001@gmail.com>
2022-01-18 00:08:10 +0600

    Setup BestSellers in the frontend

--- client/src/components/home/BestSellers.js
+++ client/src/components/home/BestSellers.js
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { getProducts } from '../../functions/product';
+import { getProducts, getProductsCount } from '../../functions/product';
 import LoadingCard from '../cards/LoadingCard';
 import ProductCard from '../cards/ProductCard';
+import { Pagination } from 'antd';
 
 const BestSellers = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [productsCount, setProductsCount] = useState(0);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     loadAllProducts();
+  }, [page]);
+
+  useEffect(() => {
+    getProductsCount().then(res => setProductsCount(res.data));
   }, []);
 
   const loadAllProducts = () => {
     setLoading(true);
-    getProducts('sold', 'desc', 3).then(res => {
+    getProducts('sold', 'desc', page).then(res => {
       setProducts(res.data);
       setLoading(false);
     });
@@ -33,6 +40,19 @@ const BestSellers = () => {
             ))}
           </div>
         )}
+      </div>
+
+      <div className="row">
+        <nav
+          className="col-md-12"
+          style={{ textAlign: 'center', marginTop: '30px' }}
+        >
+          <Pagination
+            current={page}
+            total={Math.ceil(productsCount / 3) * productsCount}
+            onChange={value => setPage(value)}
+          />
+        </nav>
       </div>
     </>
   );
