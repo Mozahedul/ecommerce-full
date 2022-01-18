2df63f6e5bf1fa8e3688afeba67371bc580a395c
Mozahedul <mozahed001@gmail.com>
2022-01-18 21:04:20 +0600

    Customize product image in product details page

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { Card } from 'antd';
 import { Link } from 'react-router-dom';
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Laptop from '../../images/laptop.jpg';
 const { Meta } = Card;
 
 const SingleProduct = ({ product }) => {
   const { title, description, images, slug } = product;
   return (
     <>
-      <div className="col-md-7">image carousel</div>
+      <div className="col-md-7">
+        {images && images.length ? (
+          <Carousel showArrows={true} autoPlay infiniteLoop>
+            {images &&
+              images.map(i => (
+                <img src={i.url} key={i.public_id} alt={title} />
+              ))}
+          </Carousel>
+        ) : (
+          <Card cover={<img src={Laptop} alt={title} />} />
+        )}
+      </div>
       <div className="col-md-5">
         <Card
           actions={[
