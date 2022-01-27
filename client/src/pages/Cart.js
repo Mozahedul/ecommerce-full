045172cb797998fb5bf6a1ddf2f8ce0d2747bb2b
Mozahedul <mozahed001@gmail.com>
2022-01-27 21:54:22 +0600

    Calcualte product price total & proceed to checkout & login to checkout button created

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
   const { cart, user } = useSelector(state => ({ ...state }));
+
+  const getTotal = () =>
+    cart.reduce(
+      (currentValue, nextValue) =>
+        currentValue + nextValue.count * nextValue.price,
+      0
+    );
   return (
-    <div className="container-fluid">
+    <div className="container-fluid m-t-2">
       <div className="row">
-        <h4>Cart</h4>
-        {JSON.stringify(cart)}
+        <div className="col-md-8">
+          <h4>Cart / {cart.length} product</h4>
+          {!cart.length ? (
+            <p>
+              No products in cart. <Link to="/shop">Continue Shopping</Link>
+            </p>
+          ) : (
+            <h4>show cart items</h4>
+          )}
+        </div>
+        <div className="col-md-4">
+          <h4>Order Summary</h4>
+          <hr />
+          <p>Products</p>
+          {cart.map((c, i) => (
+            <div key={i}>
+              <p>
+                {c.title} x {c.count} = ${c.price * c.count}
+              </p>
+            </div>
+          ))}
+          <hr />
+          <div>
+            Total: <b>${getTotal()}</b>
+          </div>
+          <hr />
+          {user ? (
+            <button className="btn btn-sm btn-primary mt-2">
+              Proceed to Checkout
+            </button>
+          ) : (
+            <button className="btn btn-sm btn-primary mt-2">
+              Login to Checkout
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
