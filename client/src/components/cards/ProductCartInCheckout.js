3483ef6b18250eb8692da807521d3c5a5237c237
Mozahedul <mozahed001@gmail.com>
2022-01-28 17:39:17 +0600

    Show product image in Cart page with react-modal-image npm package

--- client/src/components/cards/ProductCartInCheckout.js
+++ client/src/components/cards/ProductCartInCheckout.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import ModalImage from 'react-modal-image';
+import laptop from '../../images/laptop.jpg';
 
 const ProductCartInCheckout = ({ p }) => {
   return (
     <tbody>
       <tr>
-        <td>Image</td>
+        <td>
+          <div style={{ width: '80px', height: 'auto' }}>
+            {p.images.length ? (
+              <ModalImage small={p.images[0].url} large={p.images[0].url} />
+            ) : (
+              <ModalImage small={laptop} large={laptop} />
+            )}
+          </div>
+        </td>
         <td>{p.title}</td>
         <td>${p.price}</td>
         <td>{p.brand}</td>
