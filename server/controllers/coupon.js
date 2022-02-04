a93ad11ff80e235d9c6d81d0082c97f2de6da291
Mozahedul <mozahed001@gmail.com>
2022-02-04 13:21:47 +0600

    Create route & controller of coupon for create, remove & list the coupon

---
+++ server/controllers/coupon.js
@@ -0,0 +1,31 @@
+const Coupon = require('../models/coupon');
+
+// Create, remove, list
+module.exports.create = async (req, res) => {
+  try {
+    const { name, expiry, discount } = req.body;
+    const coupon = await new Coupon({ name, expiry, discount }).save();
+    res.json(coupon);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// for removing coupon
+module.exports.remove = async (req, res) => {
+  try {
+    const coupon = await Coupon.findByIdAndDelete(req.params.couponId).exec();
+    res.json(coupon);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports.list = async (req, res) => {
+  try {
+    const coupon = await Coupon.find({}).sort({ createdAt: -1 }).exec();
+    res.json(coupon);
+  } catch (error) {
+    console.log(error);
+  }
+};
