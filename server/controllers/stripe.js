61fad131fda68cddf8633b62686e787b694f21a8
Mozahedul <mozahed001@gmail.com>
2022-02-09 00:12:28 +0600

    Add cartTotal in stripe card payment

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -5,8 +5,16 @@ const Product = require('../models/product');
 const Coupon = require('../models/coupon');
 
 module.exports.createPaymentIntent = async (req, res) => {
+  // 1. find user
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  // 2. get user cart total
+  const { cartTotal } = await Cart.findOne({ orderedBy: user._id }).exec();
+
+  console.log('CART TOTAL CHARGED ==> ', cartTotal);
+  // Create payment intent with order & currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 100,
+    amount: cartTotal * 100,
     currency: 'usd',
   });
 
@@ -14,4 +22,3 @@ module.exports.createPaymentIntent = async (req, res) => {
     clientSecret: paymentIntent.client_secret,
   });
 };
- 
